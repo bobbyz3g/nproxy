9b587f7acc6a32ac3ef98f76141113100cb2edb0
Kaiser925 <zhangliang925@icloud.com>
2022-12-05 21:39:30 +0800

    :tada: init project
    Add go.mod main.go and .gitignore

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kaiser925/nproxy
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 The Author Kaiser925. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("nproxy")
+}
