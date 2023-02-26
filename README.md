f3e61aced896d9d166c568a68003c2158dc5f470
Kaiser925 <zhangliang925@icloud.com>
2023-02-26 18:20:33 +0800

    :sparkles: add nproxy tool

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nproxy
-nproxy is a simple net proxy tool.
+
+nproxy is a simple tcp net proxy tool.
+
+```
+$ go install github.com/Kaiser925/nproxy@latest
+$ nproxy 127.0.0.1:8080@remote-host:80
+```
--- main.go
+++ main.go
@@ -4,8 +4,74 @@
 
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("nproxy")
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("usage: nproxy <localhost:port@remote:port>")
+	}
+
+	done := make(chan struct{})
+
+	for _, v := range args {
+		localAddr, remoteAddr, ok := strings.Cut(v, "@")
+		if !ok {
+			log.Fatalf("error proxy argument: %s", v)
+		}
+		log.Printf("start listen %q proxy to %q", localAddr, remoteAddr)
+		go func() {
+			proxy(localAddr, remoteAddr)
+		}()
+	}
+
+	<-done
+}
+
+func proxy(localAddr, remoteAddr string) {
+	l, err := net.Listen("tcp", localAddr)
+	if err != nil {
+		log.Printf("listen %q failed: %v", localAddr, err)
+		return
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Printf("accept connection failed: %v", err)
+			return
+		}
+
+		go func() {
+			defer conn.Close()
+
+			remote, err := net.Dial("tcp", remoteAddr)
+			if err != nil {
+				log.Printf("dial %q failed: %v", localAddr, err)
+				return
+			}
+			defer remote.Close()
+
+			go func() {
+				_, err = io.Copy(remote, conn)
+				if err != nil {
+					if err != nil {
+						log.Printf("copy data from remote to local failed: %v", err)
+					}
+				}
+			}()
+
+			_, err = io.Copy(conn, remote)
+			if err != nil {
+				log.Printf("copy data from local to remote failed: %v", err)
+			}
+		}()
+
+	}
 }
